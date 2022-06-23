f028f2d259316cebf38959b8084816b0b0a05c7d
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-06-23 23:29:29 +0100

    store session in db

--- SignalRTestDotNet/Middleware/AdminStartMiddleware.cs
+++ SignalRTestDotNet/Middleware/AdminStartMiddleware.cs
@@ -8,15 +8,19 @@ namespace SignalRTestDotNet.Middleware;
 public class AdminStartMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly GameContext _gameContext;
 
-    public AdminStartMiddleware(RequestDelegate next, GameContext gameContext)
+    /*
+     * Middleware are singletons, and cannot be injected with scoped services
+     * Scoped services must be injected to the InvokeAsync method
+     */
+
+
+    public AdminStartMiddleware(RequestDelegate next)
     {
         _next = next;
-        _gameContext = gameContext;
     }
 
-    public async Task InvokeAsync(HttpContext context)
+    public async Task InvokeAsync(HttpContext context, GameContext gameContext)
     {
         // New session started on client by deleting 'AdminId' cookie. Hence a new 'AdminId' cookie
         // is generated here only when absent
@@ -24,13 +28,18 @@ public class AdminStartMiddleware
         {
             var adminId = Guid.NewGuid().ToString();
             var sessionId = Guid.NewGuid().ToString();
+
+            // store in database
+            var session = new Session { AdminId = adminId, SessionId = sessionId };
+            var sess = await gameContext.Sessions.AddAsync(session);
+            await gameContext.SaveChangesAsync();
+            Console.WriteLine(session);
+
+            // if database save successful return cookies to user
             context.Response.Cookies.Append("AdminId", adminId);
             context.Response.Cookies.Append("SessionId", sessionId);
-            // store in database
-            
-            var session = new Session{AdminId = adminId, SessionId = sessionId};
-            _gameContext.Add(session);
-            
+
+
 
         }
 
