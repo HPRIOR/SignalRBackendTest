8c4d96921f5cb92f3c0e341b191b37d832e27766
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-06-23 20:44:17 +0100

    store session in db

--- SignalRTestDotNet/Middleware/AdminStartMiddleware.cs
+++ SignalRTestDotNet/Middleware/AdminStartMiddleware.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using SignalRTestDotNet.GameContextNs;
 
 namespace SignalRTestDotNet.Middleware;
 
 public class AdminStartMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly GameContext _gameContext;
 
-    public AdminStartMiddleware(RequestDelegate next)
+    public AdminStartMiddleware(RequestDelegate next, GameContext gameContext)
     {
         _next = next;
+        _gameContext = gameContext;
     }
 
     public async Task InvokeAsync(HttpContext context)
@@ -19,9 +22,16 @@ public class AdminStartMiddleware
         // is generated here only when absent
         if (context.Request.Path == "/hub" && !context.Request.Cookies.ContainsKey("AdminId"))
         {
-            context.Response.Cookies.Append("AdminId", Guid.NewGuid().ToString());
-            context.Response.Cookies.Append("SessionId", Guid.NewGuid().ToString());
+            var adminId = Guid.NewGuid().ToString();
+            var sessionId = Guid.NewGuid().ToString();
+            context.Response.Cookies.Append("AdminId", adminId);
+            context.Response.Cookies.Append("SessionId", sessionId);
             // store in database
+            
+            var session = new Session{AdminId = adminId, SessionId = sessionId};
+            _gameContext.Add(session);
+            
+
         }
 
         await _next(context);
