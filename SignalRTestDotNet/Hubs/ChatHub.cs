3b33905770db72ffebb9cc3d98645ffe3af37f23
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-07-18 07:48:00 +0100

    Send message to players instead of using group

--- SignalRTestDotNet/Hubs/ChatHub.cs
+++ SignalRTestDotNet/Hubs/ChatHub.cs
@@ -17,12 +17,11 @@ public class ChatHub : Hub
         Context.Abort();
     }
 
-    public async Task<bool> VerifyPlayerSession(string sessionId, string playerId, string userId)
+    public async Task VerifyPlayerSession(string sessionId, string playerId, string userId)
     {
         if (playerId != userId)
         {
             await AbortSessionWith("User id does not match player id");
-            return false;
         }
 
         var session = await _gameContext.FindAsync<Session>(sessionId);
@@ -31,31 +30,26 @@ public class ChatHub : Hub
         if (session is null || player is null || session.SessionId != player.SessionId)
         {
             await AbortSessionWith("No matching session or player found");
-            return false;
         }
 
         Console.WriteLine("player verified");
         await Clients.Caller.SendAsync("verifiedSession", "Player");
-        return true;
     }
 
-    private async Task<bool> VerifyAdminSession(String sessionId, string adminId, string userId)
+    private async Task VerifyAdminSession(String sessionId, string adminId, string userId)
     {
         if (adminId != userId)
         {
             await AbortSessionWith("No matching user id for admin id");
-            return false;
         }
 
         var session = await _gameContext.FindAsync<Session>(sessionId);
         if (session is null || session.AdminId != adminId)
         {
             await AbortSessionWith("No matching session for admin");
-            return false;
         }
 
         await Clients.Caller.SendAsync("verifiedSession", "Admin");
-        return true;
     }
 
     public override async Task OnConnectedAsync()
@@ -64,26 +58,17 @@ public class ChatHub : Hub
         var sessionQuery = Context.GetHttpContext()!.Request.Query["session"];
         var userId = Context.UserIdentifier;
 
-        var isPlayerSession = (!String.IsNullOrEmpty(sessionQuery) && !String.IsNullOrEmpty(sessionQuery));
+        var isPlayerSession =  !string.IsNullOrEmpty(sessionQuery) && !string.IsNullOrEmpty(sessionQuery) ;
         if (isPlayerSession)
         {
             if (userId != playerQuery) return;
-            var verified = await VerifyPlayerSession(sessionQuery, playerQuery, userId);
-            if (verified)
-            {
-                Console.WriteLine("Adding player to group");
-                await Groups.AddToGroupAsync(Context.UserIdentifier, sessionQuery);
-            }
+            await VerifyPlayerSession(sessionQuery, playerQuery, userId);
         }
         else // verify admin
         {
             var adminCookie = Context.GetHttpContext()?.Request.Cookies["AdminId"];
             var sessionCookie = Context.GetHttpContext()?.Request.Cookies["SessionId"];
-            var verified = await VerifyAdminSession(sessionCookie, adminCookie, userId);
-            if (verified){
-                Console.WriteLine("Adding admin to group");
-                await Groups.AddToGroupAsync(Context.UserIdentifier, sessionCookie);
-            }
+            await VerifyAdminSession(sessionCookie, adminCookie, userId);
         }
 
         await base.OnConnectedAsync();
@@ -93,9 +78,9 @@ public class ChatHub : Hub
     private readonly GameContext _gameContext;
 
     // will get from config in the real thing
-    private const string _clientUrl = "http://localhost:8080/";
+    private const string ClientUrl = "http://localhost:8080/";
 
-    public ChatHub(GameContextNs.GameContext gameContext)
+    public ChatHub(GameContext gameContext)
     {
         _gameContext = gameContext;
     }
@@ -106,12 +91,14 @@ public class ChatHub : Hub
         await Clients.All.SendAsync("messageReceived", username, message);
     }
 
-    /*
-     * Groups will be associated with sessionIds.  Only admins will be able to send messages to the group
-     * by using their admin id.
-     */
-    public async Task SendMessageToGroup(string sessionId, string message)
+    public async Task SendMessageToPlayers(string sessionId, string message)
     {
+        _gameContext
+            .Players
+            .Where(player => player.SessionId == sessionId)
+            .ToList()
+            .ForEach(async player => { await Clients.User(player.PlayerId).SendAsync("SendMessage", message); });
+
         await Clients.Group(sessionId).SendAsync("SendMessage", message);
     }
 
@@ -122,7 +109,6 @@ public class ChatHub : Hub
 
     public async Task SendMessageToAdmin(string sessionId, string message)
     {
-        System.Console.WriteLine("message admin");
         var session = await _gameContext.FindAsync<Session>(sessionId);
         if (session is not null)
         {
@@ -156,7 +142,7 @@ public class ChatHub : Hub
 
                 return new PlayerDAO
                 {
-                    Url = _clientUrl + "?session=" + sessionId + "&player=" + playerId,
+                    Url = ClientUrl + "?session=" + sessionId + "&player=" + playerId,
                     Country = country,
                     SessionId = sessionId,
                     PlayerId = playerId
